cc2ba163f176cf8641f31713173be77212fd5dc3
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2022-09-03 20:41:01 +0300

    Add project files.

---
+++ RedirectWithParams/Models/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedirectWithParams.Models
+{
+    public class CustomerViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
